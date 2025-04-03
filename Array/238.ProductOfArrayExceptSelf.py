67432fed0e229c82b7f3cdb6408a23011e9d67ef
VinayakaHK <vinayakahk81@gmail.com>
2025-04-03 20:12:18 +0530

    product

--- Array/238.ProductOfArrayExceptSelf.py
+++ Array/238.ProductOfArrayExceptSelf.py
@@ -1,13 +1,38 @@
-class Solution:
-    def productExceptSelf(self, nums: List[int]) -> List[int]:
-        res = [1] * (len(nums))
+def productExceptSelf(nums):
+    # Get the length of input array
+    n = len(nums)
+    # Initialize result array with 1s
+    res = [1] * (n)
+    print("res", res)
 
-        prefix = 1
-        for i in range(len(nums)):
-            res[i] = prefix
-            prefix *= nums[i]
-        postfix = 1
-        for i in range(len(nums) -1, -1, -1):
-            res[i] *= postfix
-            postfix *= nums[i]
-        return res
+    # Calculate prefix products (products of all elements to the left)
+    prefix = 1
+    for i in range(n):
+        print("i", i)
+        # Store the product of all elements to the left of current position
+        res[i] = prefix
+        print("res[i]", res[i])
+        # Update prefix by multiplying with current number
+        prefix = prefix * nums[i]
+        print("prefix", prefix)
+
+    print('res',res)
+    print("-------")
+
+    # Calculate postfix products (products of all elements to the right)
+    postfix = 1
+    # Iterate from right to left
+    for i in range(n -1, -1, -1):
+        # Multiply current position with postfix to get final result
+        res[i]  = res[i] *  postfix
+        print("res[i]", res[i])
+        # Update postfix by multiplying with current number
+        postfix = postfix * nums[i]
+        print("postfix", postfix)
+
+    print('res',res)
+    print("-------")
+    return res
+
+# Test the function with example array [1,2,3,4]
+print(productExceptSelf([1,2,3,4]))
